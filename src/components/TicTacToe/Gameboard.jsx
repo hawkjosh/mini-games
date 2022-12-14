c84272a3afcd2d75b89fd359e5e2e5ea339b8629
hawkjosh <hawkjosh@icloud.com>
2022-12-13 19:16:43 -0500

    new push

--- src/components/TicTacToe/Gameboard.jsx
+++ src/components/TicTacToe/Gameboard.jsx
@@ -64,10 +64,10 @@ export default function Gameboard({ board, onClick }) {
       
       <Tablet>
         <Box
-          sx={{border: 'solid darkgray', borderRadius: '1rem', padding: '0.5rem'}}
+          sx={{border: 'solid darkgray', borderRadius: '1rem', padding: '0.65rem'}}
           >
           <Box
-            sx={{display: 'grid', gridTemplateColumns: 'repeat(3, 8.25rem)', placeItems: 'center', justifyContent: 'center'}}
+            sx={{display: 'grid', gridTemplateColumns: 'repeat(3, 9rem)', placeItems: 'center', justifyContent: 'center'}}
             >
             {board.map((value, index) => {
               return <Square key={index} value={value} onClick={() => value === null && onClick(index)} />
