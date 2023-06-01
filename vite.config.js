fb50b55afeb05efab3b66e6a4557026e8fcbe0ff
hawkjosh <hawkjosh@icloud.com>
2023-06-01 10:34:37 -0400

    update push 6/1/23 @ 10:34am

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [react()],
+	build: {
+		outDir: 'dist',
+		assetsDir: '',
+		rollupOptions: {
+			output: {
+				entryFileNames: '[name].js',
+				chunkFileNames: '[name].js',
+				assetFileNames: '[name].[ext]',
+				manualChunks: undefined,
+			},
+		},
+	},
+	server: {
+		fs: {
+			strict: false,
+		},
+	},
 })
